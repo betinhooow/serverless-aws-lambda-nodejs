53fb22d535b8b20c08a9ab7ad1aa4125d4711d90
betinhoow <robertonobre90@gmail.com>
2020-02-11 20:32:23 -0300

    Optmizing image, size, quality, proportion and format

--- optimize.js
+++ optimize.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-
+const sharp = require('sharp');
 const S3 = new AWS.S3();
 
 module.exports.handle = async ({ Records: records }, context) => {
@@ -13,6 +13,11 @@ module.exports.handle = async ({ Records: records }, context) => {
         Bucket: process.env.buckect,
         Key: key
       }).promise();
+
+      const optimized = await sharp(image.Body)
+        .resize(1280, 720, { fit: 'inside', withoutEnlargement: true })
+        .toFormat('jpeg', { progressive: true, quality: 50 })
+        .toBuffer()
 
     }));
 
